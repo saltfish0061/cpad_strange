be0f8662bba56a44442f5e66dc7868ef0d6e3436
CH'NG WEI FAN <chngweifan@graduate.utm.my>
2026-05-14 21:36:31 +0800

    fix db connect

--- includes/db.php
+++ includes/db.php
@@ -9,7 +9,6 @@ $db_conn = new mysqli($host, $username, $password, $database);
 
 // Check connection
 if ($db_conn->connect_error) {
-  // FIXED: Changed $conn to $db_conn
   die("Connection failed: " . $db_conn->connect_error);
 }
 
