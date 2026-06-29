67e4affab580edd24e8d088b83047e8af94dd404
CH'NG WEI FAN <chngweifan@graduate.utm.my>
2026-06-29 11:51:43 +0800

    update schema: fix password to not null, add missing payment_method attribute

--- strange-3/public/dbdocs/schema.sql
+++ strange-3/public/dbdocs/schema.sql
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS `menus` (
 CREATE TABLE IF NOT EXISTS `users` (
   `user_id` varchar(20) NOT NULL,
   `name` varchar(50) NOT NULL,
-  `password` varchar(255) DEFAULT NULL,
+  `password varchar(255) NOT NULL DEFAULT 'temporary_default_password`,
   `role` enum('admin','customer') NOT NULL DEFAULT 'customer',
   `phone` varchar(20) DEFAULT NULL,
   `address` text DEFAULT NULL,
@@ -27,6 +27,7 @@ CREATE TABLE IF NOT EXISTS `orders` (
   `total_amount` decimal(10,2) NOT NULL,
   `status` enum('pending','preparing','ready','on_the_way','completed','cancelled') DEFAULT 'pending',
   `delivery_method` enum('pickup','delivery') NOT NULL DEFAULT 'pickup',
+  `payment_method` enum('cash','credit_card','ewallet') NOT NULL DEFAULT 'cash',
   `order_note` text DEFAULT NULL,
   `order_date` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`order_id`),
